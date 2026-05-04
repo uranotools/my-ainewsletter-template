fc0e59e6a3ba9df9bcc817805b61dfcf9ea619fa
andygomezb7 <andygomezb7@gmail.com>
2026-05-04 02:01:21 -0600

    Fix fetch url to relative path

--- src/config.ts
+++ src/config.ts
@@ -3,5 +3,6 @@
 export const GITHUB_POSTS_EDIT_URL = "https://github.com/uranotools/my-ainewsletter-template/edit/main/public/data/posts.json";
 
 // URL para el archivo JSON con los datos de las noticias. 
-// Usar import.meta.env.BASE_URL asegura que funcione en GitHub Pages.
-export const POSTS_JSON_URL = `${import.meta.env.BASE_URL}data/posts.json`;
+// Al usar una ruta relativa ("./data/...") funciona perfectamente con HashRouter 
+// y no depende del nombre del repositorio en GitHub Pages.
+export const POSTS_JSON_URL = "./data/posts.json";
