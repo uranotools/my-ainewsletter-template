366a85794bffc651c8178ce9fdcac7e2fac42071
andygomezb7 <andygomezb7@gmail.com>
2026-05-07 09:52:57 -0600

    images copy

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   return {
-    plugins: [react()],
+    plugins: [
+      react(),
+      viteStaticCopy({
+        targets: [
+          { src: 'assets', dest: '.' },
+        ],
+      }),
+    ],
     base: './', // Relative base para GitHub Pages
     resolve: {
       alias: {
